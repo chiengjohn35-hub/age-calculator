758b230f97118ac26412603ead94154f439e30b5
Chieng_john <chiengbulgai35@gmail.com>
2025-09-06 10:02:15 -0400

    Update age_calculator2.py

--- age_calculator2.py
+++ age_calculator2.py
@@ -22,38 +22,3 @@ def main():
 if __name__=="__main__":
     main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def year_of_birth(n):
-#     enter = int(input("enter the year of birth :"))
-#     if not enter.is_integer():
-#         raise ValueError("Please enter a valid number or digit!")
-#     if enter:
-#         age = n - enter
-#         print(f"Your are {age} years old")
-#         return enter
-#
-# def main():
-#     while True:
-#         try:
-#             current_year = int(input("enter the current year:"))
-#             current_year = year_of_birth(current_year)
-#             break
-#         except ValueError:
-#             print("Please enter a valid number or digit!")
-#
-# if __name__ == '__main__':
-#     main()
